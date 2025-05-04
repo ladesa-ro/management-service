c186168ae4a0e173fe9ed40f43a30151202fa863
UrielAndrade <andradeleiru@gmail.com>
2025-05-04 10:31:42 -0300

    modfy add import at usuario.module from PerfilModule

--- api-service/src/application/resources/autenticacao/usuario/usuario.module.ts
+++ api-service/src/application/resources/autenticacao/usuario/usuario.module.ts
@@ -3,9 +3,10 @@ import { Module } from "@nestjs/common";
 import { UsuarioController } from "./usuario.controller";
 import { UsuarioResolver } from "./usuario.resolver";
 import { UsuarioService } from "./usuario.service";
+import { PerfilModule } from "../../autorizacao/perfil/perfil.module";
 
 @Module({
-  imports: [KeycloakModule],
+  imports: [KeycloakModule, PerfilModule],
   controllers: [UsuarioController],
   providers: [UsuarioService, UsuarioResolver],
   exports: [UsuarioService],
