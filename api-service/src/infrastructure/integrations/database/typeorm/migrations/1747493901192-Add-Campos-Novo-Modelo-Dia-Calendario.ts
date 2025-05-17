468b122b21e13ed0e4bd1c137117fbb01f26e8d2
UrielAndrade <andradeleiru@gmail.com>
2025-05-17 12:08:18 -0300

    feat/New migration for table dia_calendario

---
+++ api-service/src/infrastructure/integrations/database/typeorm/migrations/1747493901192-Add-Campos-Novo-Modelo-Dia-Calendario.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class AddCamposNovoModeloDiaCalendario1710189999999 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      "dia_calendario",
+      new TableColumn({
+        name: "dia_presencial",
+        type: "boolean",
+        isNullable: false,
+        default: false,
+      }),
+    );
+
+    // Adiciona `tipo`
+    await queryRunner.addColumn(
+      "dia_calendario",
+      new TableColumn({
+        name: "tipo",
+        type: "varchar",
+        length: "50",
+        isNullable: false,
+        default: `'Outro'`,
+      }),
+    );
+
+    // Altera `feriado` de boolean para varchar (string ou null)
+    await queryRunner.changeColumn(
+      "dia_calendario",
+      "feriado",
+      new TableColumn({
+        name: "feriado",
+        type: "varchar",
+        isNullable: true,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // Reverte as alterações
+    await queryRunner.dropColumn("dia_calendario", "dia_presencial");
+    await queryRunner.dropColumn("dia_calendario", "tipo");
+
+    await queryRunner.changeColumn(
+      "dia_calendario",
+      "feriado",
+      new TableColumn({
+        name: "feriado",
+        type: "bool",
+        isNullable: false,
+      }),
+    );
+  }
+}
