53ba605d75dc22c28cf406802ce98ed0d3b6def4
UrielAndrade <andradeleiru@gmail.com>
2025-05-20 13:32:30 -0300

    feat/New migration for add new colum "dia_calendario"

--- api-service/src/infrastructure/integrations/database/typeorm/migrations/1747493901192-Add-Campos-Novo-Modelo-Dia-Calendario.ts
+++ api-service/src/infrastructure/integrations/database/typeorm/migrations/1747493901192-Add-Campos-Novo-Modelo-Dia-Calendario.ts
@@ -24,7 +24,7 @@ export class AddCamposNovoModeloDiaCalendario1710189999999 implements MigrationI
       }),
     );
 
-    // Altera `feriado` de boolean para varchar (string ou null)
+    // Altera `feriado` de boolean para varchar 
     await queryRunner.changeColumn(
       "dia_calendario",
       "feriado",
