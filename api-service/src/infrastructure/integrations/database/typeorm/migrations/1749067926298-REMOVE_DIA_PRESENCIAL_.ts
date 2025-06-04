766ac99d2f90aba61d5f516047606042e0b4522d
UrielAndrade <andradeleiru@gmail.com>
2025-06-04 16:16:29 -0400

    Modify/drop column dia_presencial (I got the logic wrong)

---
+++ api-service/src/infrastructure/integrations/database/typeorm/migrations/1749067926298-REMOVE_DIA_PRESENCIAL_.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class REMOVEDIAPRESENCIAL_1749067926298 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
