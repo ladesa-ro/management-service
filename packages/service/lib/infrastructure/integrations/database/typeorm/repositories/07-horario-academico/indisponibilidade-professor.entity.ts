e3e47e6bdc3bd517cde529cd54a86db9b30cb43b
UrielAndrade <andradeleiru@gmail.com>
2025-08-17 16:01:34 -0400

    feat: add IndisponibilidadeProfessorEntity and update package.json for entity creation script

---
+++ packages/service/lib/infrastructure/integrations/database/typeorm/repositories/07-horario-academico/indisponibilidade-professor.entity.ts
@@ -0,0 +1,32 @@
+import { PerfilEntity } from "@/infrastructure/integrations/database/typeorm/entities/index.js";
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn, RelationId } from "typeorm";
+
+@Entity({ name: "indisponibilidade_professor" })
+export class IndisponibilidadeProfessorEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+@Column({ name: "indisponibilidade_inicio", type: "timestamptz" })
+  indisponibilidade_inicio!: Date;
+
+  @Column({ name: "indisponibilidade_termino", type: "timestamptz" })
+  indisponibilidade_termino!: Date;
+
+  @Column({ name: "motivo", type: "varchar", length: 90 })
+  motivo!: string;
+
+  @Column({ name: "date_created", type: "timestamptz" })
+  date_created!: Date;
+
+  @Column({ name: "date_updated", type: "timestamptz" })
+  date_updated!: Date;
+
+  @Column({ name: "date_deleted", type: "timestamptz", nullable: true })
+  date_deleted!: Date | null;
+
+  @ManyToOne(() => PerfilEntity, { onDelete: "CASCADE", nullable: false })
+  @JoinColumn({ name: "id_perfil_fk" })
+  perfil!: PerfilEntity;
+
+  @RelationId((it: IndisponibilidadeProfessorEntity) => it.perfil)
+  id_perfil_fk!: string;
+}
