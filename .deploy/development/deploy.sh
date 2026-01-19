62b5f46177471d89772d678422dca15b527d9771
Gabriel R. Antunes <gabrielrodantunes.dev@gmail.com>
2026-01-18 22:46:30 -0400

    chore: ci/cd

--- .deploy/development/deploy.sh
+++ .deploy/development/deploy.sh
@@ -6,6 +6,6 @@ kubectl apply -f ./ingress/api.yaml;
 kubectl apply -f ./services/api.yaml;
 
 kubectl rollout restart \
-  deployment.apps/ladesa-ro-api \
-  --namespace ladesa-ro-development \
+  deployment.apps/api \
+  --namespace ladesa \
 ;
