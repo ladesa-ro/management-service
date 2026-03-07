873418f3406d4130a0df4e5178f718d1709eb0b5
UrielAndrade <andradeleiru@gmail.com>
2026-03-07 10:43:58 -0400

    feat(migration): rename 'matricula_siap' and 'matricula_siape' columns to 'matricula' in 'usuario' table

---
+++ src/app/lib/modules/@shared/infrastructure/persistence/typeorm/migrations/1772894364770-rename-column-matricula-siap-to-matricula.ts
@@ -0,0 +1,27 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+const tableName = "usuario";
+
+export class RenameColumnMatriculaSiapToMatricula1772894364770 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    
+    const table = await queryRunner.getTable(tableName);
+
+    if (table?.findColumnByName("matricula_siap")) {
+      await queryRunner.renameColumn(tableName, "matricula_siap", "matricula");
+      return;
+    }
+
+    if (table?.findColumnByName("matricula_siape")) {
+      await queryRunner.renameColumn(tableName, "matricula_siape", "matricula");
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable(tableName);
+
+    if (table?.findColumnByName("matricula")) {
+      await queryRunner.renameColumn(tableName, "matricula", "matricula_siap");
+    }
+  }
+}
