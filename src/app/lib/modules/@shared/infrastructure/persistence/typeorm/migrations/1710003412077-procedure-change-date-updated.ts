b44b7a98ac38a486ccb850379b7c7ddef34b4649
Gabriel R. Antunes <gabrielrodantunes.dev@gmail.com>
2026-03-11 07:39:02 -0400

    feat: ProcedureEnsureChangeDateTrigger1710003412087

--- src/app/lib/modules/@shared/infrastructure/persistence/typeorm/migrations/1710003412077-procedure-change-date-updated.ts
+++ src/app/lib/modules/@shared/infrastructure/persistence/typeorm/migrations/1710003412077-procedure-change-date-updated.ts
@@ -3,15 +3,15 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class ProcedureChangeDateUpdated1710003412077 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-    CREATE OR REPLACE FUNCTION change_date_updated() RETURNS TRIGGER
-      AS
-      $BODY$
-      BEGIN
-        new.date_updated := now();
-        RETURN new;
-      END;
-      $BODY$
-      LANGUAGE plpgsql;
+      CREATE OR REPLACE FUNCTION change_date_updated() RETURNS TRIGGER
+        AS
+        $BODY$
+        BEGIN
+          new.date_updated := now();
+          RETURN new;
+        END;
+        $BODY$
+        LANGUAGE plpgsql;
     `);
   }
 
