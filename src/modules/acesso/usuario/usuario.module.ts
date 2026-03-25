ce65449686e77d5e2a933c0afa8a39fa4490fd40
Gabriel R. Antunes <gabrielrodantunes.dev@gmail.com>
2026-03-24 20:29:48 -0400

    feat(usuario): add `PerfilGraphqlResolver` and `PerfilRestController` to module
    - Integrated `PerfilGraphqlResolver` and `PerfilRestController` into the `UsuarioModule` for enhanced presentation layer support.
    - Updated `controllers` and `providers` in `usuario.module.ts` to register `perfil`-related components.

--- src/modules/acesso/usuario/usuario.module.ts
+++ src/modules/acesso/usuario/usuario.module.ts
@@ -59,6 +59,8 @@ import {
 } from "@/modules/acesso/usuario/perfil/domain/queries";
 import { IPerfilRepository } from "@/modules/acesso/usuario/perfil/domain/repositories";
 import { PerfilTypeOrmRepositoryAdapter } from "@/modules/acesso/usuario/perfil/infrastructure.database";
+import { PerfilGraphqlResolver } from "@/modules/acesso/usuario/perfil/presentation.graphql/perfil.graphql.resolver";
+import { PerfilRestController } from "@/modules/acesso/usuario/perfil/presentation.rest/perfil.rest.controller";
 import { UsuarioGraphqlResolver } from "@/modules/acesso/usuario/presentation.graphql/usuario.graphql.resolver";
 import { UsuarioRestController } from "@/modules/acesso/usuario/presentation.rest/usuario.rest.controller";
 import { UsuarioEventoRestController } from "@/modules/acesso/usuario/presentation.rest/usuario-evento.rest.controller";
@@ -75,10 +77,11 @@ import { HorarioConsultaModule } from "@/modules/horarios/horario-consulta/horar
     HorarioConsultaModule,
     CampusModule,
   ],
-  controllers: [UsuarioRestController, UsuarioEventoRestController],
+  controllers: [UsuarioRestController, UsuarioEventoRestController, PerfilRestController],
   providers: [
     NestJsPaginateAdapter,
     UsuarioGraphqlResolver,
+    PerfilGraphqlResolver,
     {
       provide: IUsuarioPermissionChecker,
       useClass: UsuarioPermissionCheckerImpl,
