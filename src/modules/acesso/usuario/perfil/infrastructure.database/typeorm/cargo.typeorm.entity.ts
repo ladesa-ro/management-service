2aef7695617358373cad952c16d11b3336b62ea2
UrielAndrade <andradeleiru@gmail.com>
2026-03-26 05:45:34 -0400

    feat(entity): create 'CargoEntity' with id and nome columns, and establish relation with PerfilEntity

---
+++ src/modules/acesso/usuario/perfil/infrastructure.database/typeorm/cargo.typeorm.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, OneToMany, PrimaryColumn, type Relation } from "typeorm";
+import { PerfilEntity } from "./perfil.typeorm.entity";
+
+@Entity("cargo")
+export class CargoEntity {
+  @PrimaryColumn("uuid")
+  id!: string;
+
+  @Column({ name: "nome", type: "text", unique: true })
+  nome!: string;
+
+  @OneToMany(() => PerfilEntity, (perfil) => perfil.cargo)
+  perfis!: Relation<PerfilEntity[]>;
+}
