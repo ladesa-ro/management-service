1009797eb06431d4fd480ed7855298497d27ae71
UrielAndrade <andradeleiru@gmail.com>
2026-03-26 06:37:09 -0400

    feat(query): update cargo property in PerfilNestedQueryResult to use CargoNestedQueryResult type

--- src/modules/acesso/usuario/perfil/domain/queries/perfil-nested.query.result.ts
+++ src/modules/acesso/usuario/perfil/domain/queries/perfil-nested.query.result.ts
@@ -1,8 +1,9 @@
 import { EntityQueryResult } from "@/domain/abstractions";
 import { CampusFindOneQueryResult } from "@/modules/ambientes/campus";
+import { CargoNestedQueryResult } from "./cargo-nested.query.result";
 
 export class PerfilNestedQueryResult extends EntityQueryResult {
   ativo!: boolean;
-  cargo!: string;
+  cargo!: CargoNestedQueryResult | null;
   campus!: CampusFindOneQueryResult;
 }
