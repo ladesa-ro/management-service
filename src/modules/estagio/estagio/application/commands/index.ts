5288280238353d9aea629cecd326611d411254ef
UrielAndrade <andradeleiru@gmail.com>
2026-04-29 13:52:09 -0400

    feat(estagio): add import bulk command handler to command index

--- src/modules/estagio/estagio/application/commands/index.ts
+++ src/modules/estagio/estagio/application/commands/index.ts
@@ -1,5 +1,5 @@
 export * from "./estagio-create.command.handler";
 
 export * from "./estagio-delete.command.handler";
-
+export * from "./estagio-import-bulk.command.handler";
 export * from "./estagio-update.command.handler";
