41a283c0da2325b1cb90d77cc71df04389ede606
UrielAndrade <andradeleiru@gmail.com>
2026-04-29 17:04:39 -0400

    feat(seed): create index file for seeds module to facilitate data and function imports

---
+++ src/infrastructure.database/seeds/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @fileoverview Arquivo de índice para o módulo de seeds
+ * Facilita as importações de dados e funções de seed
+ */
+
+export * from "./seed-data";
+export { runSeed } from "./run-seed";
+export * from "./seed-examples";
