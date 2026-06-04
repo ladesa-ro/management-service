5463eb892a5352c1982fb2a8bb6d137b49d9531a
UrielAndrade <andradeleiru@gmail.com>
2026-06-04 09:13:36 -0400

    fix(perfil): add missing export for perfil-vinculos-filtro query handler

--- src/modules/acesso/usuario/perfil/application/queries/index.ts
+++ src/modules/acesso/usuario/perfil/application/queries/index.ts
@@ -5,3 +5,4 @@ export * from "./perfil-find-all-active.query.handler";
 export * from "./perfil-find-one.query.handler";
 
 export * from "./perfil-list.query.handler";
+export * from "./perfil-vinculos-filtro.query.handler";
