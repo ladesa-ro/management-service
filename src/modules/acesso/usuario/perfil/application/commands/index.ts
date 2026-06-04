98a87a5644eedfe4bdf70ef1f9d1a5cf69f1fd7b
UrielAndrade <andradeleiru@gmail.com>
2026-06-04 01:19:45 -0400

    feat(usuario): add cargo command exports to perfil commands index

--- src/modules/acesso/usuario/perfil/application/commands/index.ts
+++ src/modules/acesso/usuario/perfil/application/commands/index.ts
@@ -1,1 +1,4 @@
+export * from "./cargo-create.command.handler";
+export * from "./cargo-delete.command.handler";
+export * from "./cargo-update.command.handler";
 export * from "./perfil-definir-perfis-ativos.command.handler";
