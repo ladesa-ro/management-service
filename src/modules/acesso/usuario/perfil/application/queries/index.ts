b9588f2921b924058de85c5d948742e7eff7aa05
UrielAndrade <andradeleiru@gmail.com>
2026-06-04 01:19:32 -0400

    feat(usuario): re-export cargo query handlers in index file

--- src/modules/acesso/usuario/perfil/application/queries/index.ts
+++ src/modules/acesso/usuario/perfil/application/queries/index.ts
@@ -1,3 +1,5 @@
+export * from "./cargo-find-one.query.handler";
+export * from "./cargo-list.query.handler";
 export * from "./perfil-find-all-active.query.handler";
 
 export * from "./perfil-find-one.query.handler";
