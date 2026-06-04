996bf951b66169434f301cadd112af8d1bf8b929
UrielAndrade <andradeleiru@gmail.com>
2026-06-04 01:19:41 -0400

    feat(usuario): re-add cargo command exports to index file

--- src/modules/acesso/usuario/perfil/domain/commands/index.ts
+++ src/modules/acesso/usuario/perfil/domain/commands/index.ts
@@ -1,3 +1,8 @@
+export * from "./cargo-create.command";
+export * from "./cargo-create.command.handler.interface";
+export * from "./cargo-delete.command";
+export * from "./cargo-delete.command.handler.interface";
+export * from "./cargo-update.command";
+export * from "./cargo-update.command.handler.interface";
 export * from "./perfil-definir-perfis-ativos.command";
-
 export * from "./perfil-definir-perfis-ativos.command.handler.interface";
