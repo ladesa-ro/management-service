0f6b2952632383b417f74acc9e9c793fa97353a6
UrielAndrade <andradeleiru@gmail.com>
2026-06-07 17:20:28 -0400

    feat: refactor Redis client initialization to use redis package

--- src/server/plugins/redis-io.adapter.ts
+++ src/server/plugins/redis-io.adapter.ts
@@ -1,6 +1,6 @@
 import { IoAdapter } from "@nestjs/platform-socket.io";
 import { createAdapter } from "@socket.io/redis-adapter";
-import { Redis } from "ioredis";
+import { createClient } from "redis";
 import type { ServerOptions } from "socket.io";
 
 export class RedisIoAdapter extends IoAdapter {
@@ -14,11 +14,15 @@ export class RedisIoAdapter extends IoAdapter {
     }
 
     const redisPort = parseInt(process.env.REDIS_PORT || "6379", 10);
-    const pubClient = new Redis({ host: redisHost, port: redisPort });
+    const pubClient = createClient({ url: `redis://${redisHost}:${redisPort}` });
     const subClient = pubClient.duplicate();
 
+    await Promise.all([pubClient.connect(), subClient.connect()]);
+
     this.adapterConstructor = createAdapter(pubClient, subClient);
-    console.log(`RedisIoAdapter: Conectado ao Redis em ${redisHost}:${redisPort} para WebSocket pub/sub.`);
+    console.log(
+      `RedisIoAdapter: Conectado ao Redis em ${redisHost}:${redisPort} para WebSocket pub/sub.`,
+    );
   }
 
   createIOServer(port: number, options?: ServerOptions): any {
