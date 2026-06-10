3212b0736f5a60d030142a1306705f4678be085e
UrielAndrade <andradeleiru@gmail.com>
2026-06-10 15:22:42 -0400

    feat: add metadata for EnderecoFindOneQuery operation

--- src/modules/localidades/endereco/domain/queries/endereco-find-one.query.handler.interface.ts
+++ src/modules/localidades/endereco/domain/queries/endereco-find-one.query.handler.interface.ts
@@ -1,6 +1,12 @@
 import type { IQueryHandler } from "@/domain/abstractions";
+import { createOperationMetadata } from "@/domain/abstractions";
 import type { EnderecoFindOneQuery } from "./endereco-find-one.query";
 import type { EnderecoFindOneQueryResult } from "./endereco-find-one.query.result";
+
+export const EnderecoFindOneQueryMetadata = createOperationMetadata({
+  operationId: "enderecoFindOne",
+  summary: "Busca um endereço pelo ID",
+});
 
 export const IEnderecoFindOneQueryHandler = Symbol("IEnderecoFindOneQueryHandler");
 
