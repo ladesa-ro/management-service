3ecc87be58b47b285ab13f066c4793ff67aef471
UrielAndrade <andradeleiru@gmail.com>
2026-06-10 15:22:38 -0400

    feat: enhance EnderecoListQuery with filterCidadeId and shared fields

--- src/modules/localidades/endereco/domain/queries/endereco-list.query.ts
+++ src/modules/localidades/endereco/domain/queries/endereco-list.query.ts
@@ -1,4 +1,14 @@
-import { type IFilterAcceptableValues, PaginationQuery } from "@/domain/abstractions";
+import {
+  createFieldMetadata,
+  type IFilterAcceptableValues,
+  PaginationQuery,
+  SharedListFields,
+} from "@/domain/abstractions";
+
+export const EnderecoListQueryFields = {
+  ...SharedListFields,
+  filterCidadeId: createFieldMetadata({ description: "Filtro por ID da Cidade", nullable: true }),
+};
 
 export class EnderecoListQuery extends PaginationQuery {
   "filter.id"?: IFilterAcceptableValues;
