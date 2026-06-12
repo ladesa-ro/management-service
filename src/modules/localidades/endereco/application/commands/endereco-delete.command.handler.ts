7e516151ebb02b2bb1c2a935693e2a2a80abf98f
UrielAndrade <andradeleiru@gmail.com>
2026-06-12 14:12:33 -0400

    fix: reorder import statement and format execute method parameters for consistency

--- src/modules/localidades/endereco/application/commands/endereco-delete.command.handler.ts
+++ src/modules/localidades/endereco/application/commands/endereco-delete.command.handler.ts
@@ -1,11 +1,11 @@
 import { ensureExists } from "@/application/errors";
 import type { IAccessContext } from "@/domain/abstractions";
 import { Dep, Impl } from "@/domain/dependency-injection";
-import { Endereco } from "@/modules/localidades/endereco/domain/endereco";
 import {
   type EnderecoDeleteCommand,
   IEnderecoDeleteCommandHandler,
 } from "@/modules/localidades/endereco/domain/commands";
+import { Endereco } from "@/modules/localidades/endereco/domain/endereco";
 import { IEnderecoRepository } from "@/modules/localidades/endereco/domain/repositories";
 
 @Impl()
@@ -15,7 +15,10 @@ export class EnderecoDeleteCommandHandlerImpl implements IEnderecoDeleteCommandH
     private readonly repository: IEnderecoRepository,
   ) {}
 
-  async execute(accessContext: IAccessContext | null, command: EnderecoDeleteCommand): Promise<boolean> {
+  async execute(
+    accessContext: IAccessContext | null,
+    command: EnderecoDeleteCommand,
+  ): Promise<boolean> {
     const exists = await this.repository.exists(command.id);
     ensureExists(exists, Endereco.entityName, command.id);
     await this.repository.softDeleteById(command.id);
