ca6ed728f87b46608636f6c25ba9de6093cd2b11
UrielAndrade <andradeleiru@gmail.com>
2026-06-25 13:52:19 -0400

    feat: implement WhatsApp webhook controller and replace QR code login redirect with direct status endpoint

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { OpenWAModule } from "@/integrations/openwa/openwa.module";
 import { WhatsappNotificationsController } from "./controllers/whatsapp-notifications.controller";
+import { WhatsappWebhooksController } from "./controllers/whatsapp-webhooks.controller";
 import { WhatsappNotificationsService } from "./services/whatsapp-notifications.service";
 
 @Module({
   imports: [OpenWAModule],
-  controllers: [WhatsappNotificationsController],
+  controllers: [WhatsappNotificationsController, WhatsappWebhooksController],
   providers: [WhatsappNotificationsService],
 })
 export class NotificationsModule {}
