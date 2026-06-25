7d6742185d4cf64d6878bb3b759d26f19884c54b
UrielAndrade <andradeleiru@gmail.com>
2026-06-25 13:35:01 -0400

    feat: implement WhatsApp notification module with controller, service, and unit tests

--- src/integrations/openwa/exceptions/openwa.exceptions.ts
+++ src/integrations/openwa/exceptions/openwa.exceptions.ts
@@ -1,19 +1,19 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
+import { HttpException, HttpStatus } from "@nestjs/common";
 
 export class OpenWAConnectionException extends HttpException {
-  constructor(message = 'Failed to connect to OpenWA service') {
+  constructor(message = "Failed to connect to OpenWA service") {
     super(message, HttpStatus.SERVICE_UNAVAILABLE);
   }
 }
 
 export class OpenWATimeoutException extends HttpException {
-  constructor(message = 'OpenWA service request timed out') {
+  constructor(message = "OpenWA service request timed out") {
     super(message, HttpStatus.GATEWAY_TIMEOUT);
   }
 }
 
 export class OpenWASendMessageException extends HttpException {
-  constructor(message = 'Failed to send message via OpenWA') {
+  constructor(message = "Failed to send message via OpenWA") {
     super(message, HttpStatus.BAD_REQUEST);
   }
 }
