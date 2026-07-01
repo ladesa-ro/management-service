5ce70ea004dff36273427bba923cd73251f0e9c2
UrielAndrade <andradeleiru@gmail.com>
2026-07-01 16:02:02 -0400

    feat: define IWhatsAppProvider interface and implement provider injection for OpenWAService

--- src/integrations/openwa/openwa.module.ts
+++ src/integrations/openwa/openwa.module.ts
@@ -1,6 +1,7 @@
 import { HttpModule } from "@nestjs/axios";
 import { Module } from "@nestjs/common";
 import { AppConfigModule } from "@/infrastructure.config";
+import { IWhatsAppProviderToken } from "@/notifications/interfaces/whatsapp-provider.interface";
 import { OpenWAClient } from "./client/openwa.client";
 import { OpenWAService } from "./services/openwa.service";
 
@@ -12,7 +13,13 @@ import { OpenWAService } from "./services/openwa.service";
     }),
     AppConfigModule,
   ],
-  providers: [OpenWAClient, OpenWAService],
-  exports: [OpenWAService],
+  providers: [
+    OpenWAClient,
+    {
+      provide: IWhatsAppProviderToken,
+      useClass: OpenWAService,
+    },
+  ],
+  exports: [IWhatsAppProviderToken],
 })
 export class OpenWAModule {}
