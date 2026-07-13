3007c5a973b3e2d121a14e50c26b5fcd3e080769
UrielAndrade <andradeleiru@gmail.com>
2026-07-13 11:55:51 -0400

    feat: add deployment for WAHA (WhatsApp HTTP API) in the production ambient

--- .deploy/development/deploy.sh
+++ .deploy/development/deploy.sh
@@ -4,7 +4,23 @@ set -xe;
 
 K8S_NAMESPACE=ladesa-ro-development
 K8S_DEPLOYMENT=ladesa-ro-api
+K8S_DEPLOYMENT_WAHA=ladesa-ro-waha
 
+# Deploy do WAHA (WhatsApp HTTP API)
+helm upgrade -i ${K8S_DEPLOYMENT_WAHA} \
+  --repo https://stakater.github.io/stakater-charts \
+  application \
+  --version 6.0.2 \
+  --namespace=${K8S_NAMESPACE} \
+  -f ./values-waha.yml \
+;
+
+kubectl rollout status \
+  deployment.apps/${K8S_DEPLOYMENT_WAHA} \
+  --namespace ${K8S_NAMESPACE} \
+  --timeout=720s;
+
+# Deploy da API
 helm upgrade -i ${K8S_DEPLOYMENT} \
   --repo https://stakater.github.io/stakater-charts \
   application \
