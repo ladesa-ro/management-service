86c579320a309f35f68385f201384fc44bacf125
UrielAndrade <andradeleiru@gmail.com>
2026-07-27 19:38:49 -0400

    feat: create index file for folha-ponto domain exports

---
+++ src/modules/estagio/folha-ponto/domain/index.ts
@@ -0,0 +1,7 @@
+export * from "./commands";
+export * from "./folha-ponto";
+export * from "./folha-ponto.fields";
+export * from "./folha-ponto.schemas";
+export * from "./folha-ponto-token";
+export * from "./queries";
+export * from "./repositories";
