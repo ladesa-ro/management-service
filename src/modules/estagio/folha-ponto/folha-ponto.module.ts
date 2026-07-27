b3616b0172d642db9d3e4902e53da47c8e174536
UrielAndrade <andradeleiru@gmail.com>
2026-07-27 13:28:37 -0400

    feat: create FolhaPonto module with command handlers and services

---
+++ src/modules/estagio/folha-ponto/folha-ponto.module.ts
@@ -0,0 +1,76 @@
+import { Module } from "@nestjs/common";
+import { ScheduleModule } from "@nestjs/schedule";
+import { NestJsPaginateAdapter } from "@/infrastructure.database/pagination/adapters/nestjs-paginate.adapter";
+import { WahaModule } from "@/integrations/waha/waha.module";
+import { NotificacaoModule } from "@/modules/acesso/notificacao/notificacao.module";
+import { EstagioModule } from "@/modules/estagio/estagio/estagio.module";
+import { NotificationsModule } from "@/notifications/notifications.module";
+// Application Handlers
+import {
+  FolhaPontoCancelCommandHandlerImpl,
+  FolhaPontoCreateCommandHandlerImpl,
+  FolhaPontoTokenConfirmHandler,
+} from "./application/commands";
+import { FolhaPontoNotificacaoConsumer } from "./application/consumers";
+import { FolhaPontoExpiracaoJob } from "./application/jobs";
+import {
+  FolhaPontoFindOneQueryHandlerImpl,
+  FolhaPontoListQueryHandlerImpl,
+} from "./application/queries";
+// Services, Jobs, Consumers
+import { FolhaPontoLinkService, FolhaPontoWhatsappService } from "./application/services";
+// Domain Ports
+import {
+  IFolhaPontoCancelCommandHandler,
+  IFolhaPontoCreateCommandHandler,
+} from "./domain/commands";
+import { IFolhaPontoFindOneQueryHandler, IFolhaPontoListQueryHandler } from "./domain/queries";
+import { IFolhaPontoRepository, IFolhaPontoTokenRepository } from "./domain/repositories";
+// Infrastructure Adapters
+import {
+  FolhaPontoTokenTypeOrmRepositoryAdapter,
+  FolhaPontoTypeOrmRepositoryAdapter,
+} from "./infrastructure.database";
+// Resolvers & Controllers
+import { FolhaPontoGraphqlResolver } from "./presentation.graphql";
+import { FolhaPontoTokenRestController } from "./presentation.rest";
+
+@Module({
+  imports: [
+    ScheduleModule.forRoot(), // para o CronJob
+    EstagioModule, // acesso ao IEstagioRepository
+    NotificacaoModule, // WebSocket push (se necessário no futuro)
+    WahaModule, // IWhatsAppProvider (se usado diretamente)
+    NotificationsModule, // WhatsappNotificationsService
+  ],
+  controllers: [FolhaPontoTokenRestController],
+  providers: [
+    NestJsPaginateAdapter,
+
+    // Presentation
+    FolhaPontoGraphqlResolver,
+
+    // Background / Jobs
+    FolhaPontoExpiracaoJob,
+    FolhaPontoNotificacaoConsumer,
+
+    // Services / Utils
+    FolhaPontoLinkService,
+    FolhaPontoWhatsappService,
+
+    // Command Handlers (sem DI port)
+    FolhaPontoTokenConfirmHandler,
+
+    // Dependency Injection Bindings
+    { provide: IFolhaPontoRepository, useClass: FolhaPontoTypeOrmRepositoryAdapter },
+    { provide: IFolhaPontoTokenRepository, useClass: FolhaPontoTokenTypeOrmRepositoryAdapter },
+
+    { provide: IFolhaPontoCreateCommandHandler, useClass: FolhaPontoCreateCommandHandlerImpl },
+    { provide: IFolhaPontoCancelCommandHandler, useClass: FolhaPontoCancelCommandHandlerImpl },
+
+    { provide: IFolhaPontoListQueryHandler, useClass: FolhaPontoListQueryHandlerImpl },
+    { provide: IFolhaPontoFindOneQueryHandler, useClass: FolhaPontoFindOneQueryHandlerImpl },
+  ],
+  exports: [IFolhaPontoRepository], // Exporta o port caso outros módulos precisem
+})
+export class FolhaPontoModule {}
