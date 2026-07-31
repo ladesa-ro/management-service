0899873589ccdcc40609c87d911b79e713953d81
UrielAndrade <andradeleiru@gmail.com>
2026-07-31 18:02:22 -0400

    fix: cast query result to any in findAll method to resolve type issues

--- src/modules/estagio/folha-ponto/presentation.rest/folha-ponto.rest.controller.ts
+++ src/modules/estagio/folha-ponto/presentation.rest/folha-ponto.rest.controller.ts
@@ -57,7 +57,7 @@ export class FolhaPontoRestController {
   ): Promise<FolhaPontoListOutputRestDto> {
     const query = FolhaPontoRestMapper.listInputDtoToListQuery.map(dto);
     const queryResult = await this.listHandler.execute(accessContext, query);
-    return FolhaPontoRestMapper.listQueryResultToListOutputDto(queryResult);
+    return FolhaPontoRestMapper.listQueryResultToListOutputDto(queryResult as any);
   }
 
   @Get("/:id")
