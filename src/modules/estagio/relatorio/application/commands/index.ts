e577964c5cd2115e8ba59e7d2cbbdb82b18c1188
UrielAndrade <andradeleiru@gmail.com>
2026-08-26 08:45:51 -0400

    feat(relatorio): export application command handlers

---
+++ src/modules/estagio/relatorio/application/commands/index.ts
@@ -0,0 +1,4 @@
+export * from "./relatorio-create.command.handler";
+export * from "./relatorio-delete.command.handler";
+export * from "./relatorio-update.command.handler";
+export * from "./relatorio-upload-pdf.command.handler";
