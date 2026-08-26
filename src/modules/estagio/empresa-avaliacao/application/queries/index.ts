f6e8de53001740e8d5d9c3b4a69891d64d1df9f1
Uriel <155118208+UrielAndrade@users.noreply.github.com>
2026-08-26 18:46:43 +0000

    feat(empresa-avaliacao): export application queries index

---
+++ src/modules/estagio/empresa-avaliacao/application/queries/index.ts
@@ -0,0 +1,5 @@
+export * from "./empresa-avaliacao-find-my.query.handler";
+export * from "./empresa-avaliacao-find-one.query.handler";
+export * from "./empresa-avaliacao-historico-list.query.handler";
+export * from "./empresa-avaliacao-list.query.handler";
+export * from "./empresa-score-find-one.query.handler";
