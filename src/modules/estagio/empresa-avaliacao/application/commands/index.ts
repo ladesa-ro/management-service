ec95dc04275e2cc8d401438d41165d27b1a7bd7a
Uriel <155118208+UrielAndrade@users.noreply.github.com>
2026-08-26 18:46:43 +0000

    feat(empresa-avaliacao): export application commands index

---
+++ src/modules/estagio/empresa-avaliacao/application/commands/index.ts
@@ -0,0 +1,6 @@
+export * from "./empresa-avaliacao-create.command.handler";
+export * from "./empresa-avaliacao-delete.command.handler";
+export * from "./empresa-avaliacao-like.command.handler";
+export * from "./empresa-avaliacao-unlike.command.handler";
+export * from "./empresa-avaliacao-update.command.handler";
+export * from "./empresa-score-recalculate.command.handler";
