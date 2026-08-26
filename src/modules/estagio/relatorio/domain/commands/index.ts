ff9000faed7b94ab26da074272eebe575e44c50c
UrielAndrade <andradeleiru@gmail.com>
2026-08-26 08:45:50 -0400

    feat(relatorio): export domain commands

---
+++ src/modules/estagio/relatorio/domain/commands/index.ts
@@ -0,0 +1,6 @@
+export * from "./relatorio-create.command";
+export * from "./relatorio-create.command.handler.interface";
+export * from "./relatorio-delete.command.handler.interface";
+export * from "./relatorio-update.command";
+export * from "./relatorio-update.command.handler.interface";
+export * from "./relatorio-upload-pdf.command.handler.interface";
