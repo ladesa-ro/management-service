4492ffc7d9f15f11af31199e468fafdf202dae86
Uriel <155118208+UrielAndrade@users.noreply.github.com>
2026-09-04 19:45:57 +0000

    feat(estagio): expand filterable and searchable columns in relatorio pagination spec

--- src/modules/estagio/relatorio/domain/queries/relatorio-pagination-spec.ts
+++ src/modules/estagio/relatorio/domain/queries/relatorio-pagination-spec.ts
@@ -5,7 +5,15 @@ export const relatorioPaginationSpec: IPaginationSpec = {
   filterableColumns: {
     id: [PaginationFilter.EQ],
     "estagio.id": [PaginationFilter.EQ],
+    "estagio.empresa.id": [PaginationFilter.EQ],
+    "estagio.status": [PaginationFilter.EQ, PaginationFilter.IN],
+    "estagio.estagiario.id": [PaginationFilter.EQ],
+    "estagio.estagiario.perfil.usuario.matricula": [PaginationFilter.EQ],
+    "estagio.estagiario.perfil.usuario.nome": [PaginationFilter.EQ, PaginationFilter.ILIKE],
   },
-  searchableColumns: [],
+  searchableColumns: [
+    "estagio.estagiario.perfil.usuario.nome",
+    "estagio.estagiario.perfil.usuario.matricula",
+  ],
   sortableColumns: ["dateCreated", "dateUpdated"],
 };
