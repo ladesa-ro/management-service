d09cadad73d66ac7dc03c708cb70c56a08aae4e6
Uriel <155118208+UrielAndrade@users.noreply.github.com>
2026-09-04 19:45:05 +0000

    feat(estagio): add zod schemas for new empresa query filters

--- src/modules/estagio/empresa/domain/queries/empresa-list.query.schemas.ts
+++ src/modules/estagio/empresa/domain/queries/empresa-list.query.schemas.ts
@@ -14,11 +14,29 @@ import {
 export const EmpresaPaginationInputSchema = createPaginationInputSchema({
   "filter.cnpj": stringFilterSchema,
   "filter.nomeFantasia": stringFilterSchema,
+  "filter.razaoSocial": stringFilterSchema,
+  "filter.email": stringFilterSchema,
+  "filter.telefone": stringFilterSchema,
   "filter.endereco.id": stringFilterSchema,
+  "filter.endereco.cidade.id": stringFilterSchema,
+  "filter.cidade.id": stringFilterSchema,
+  "filter.endereco.cidade.nome": stringFilterSchema,
+  "filter.cidade.nome": stringFilterSchema,
+  "filter.endereco.cidade.estado.id": stringFilterSchema,
+  "filter.estado.id": stringFilterSchema,
+  "filter.endereco.cidade.estado.sigla": stringFilterSchema,
+  "filter.estado.sigla": stringFilterSchema,
 });
 
 export const EmpresaGraphqlListInputSchema = createGraphqlListInputSchema({
   filterCnpj: z.array(z.string()).optional(),
   filterNomeFantasia: z.array(z.string()).optional(),
+  filterRazaoSocial: z.array(z.string()).optional(),
+  filterEmail: z.array(z.string()).optional(),
+  filterTelefone: z.array(z.string()).optional(),
   filterEnderecoId: z.array(z.string()).optional(),
+  filterCidadeId: z.array(z.string()).optional(),
+  filterCidadeNome: z.array(z.string()).optional(),
+  filterEstadoId: z.array(z.string()).optional(),
+  filterEstadoSigla: z.array(z.string()).optional(),
 });
