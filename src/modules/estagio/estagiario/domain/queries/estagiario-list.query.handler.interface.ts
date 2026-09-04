dbd45fdd1968462863ee80bc572165362e8b3e9b
Uriel <155118208+UrielAndrade@users.noreply.github.com>
2026-09-04 19:43:33 +0000

    feat(estagio): add student and course filter columns to estagiario pagination spec

--- src/modules/estagio/estagiario/domain/queries/estagiario-list.query.handler.interface.ts
+++ src/modules/estagio/estagiario/domain/queries/estagiario-list.query.handler.interface.ts
@@ -19,11 +19,25 @@ export type IEstagiarioListQueryHandler = IQueryHandler<
 
 export const estagiarioPaginationSpec: IPaginationSpec = {
   sortableColumns: ["telefone", "dataNascimento", "dateCreated"],
-  searchableColumns: ["telefone", "emailInstitucional", "periodo"],
+  searchableColumns: [
+    "telefone",
+    "emailInstitucional",
+    "periodo",
+    "perfil.usuario.nome",
+    "perfil.usuario.matricula",
+    "perfil.usuario.email",
+  ],
   defaultSortBy: [["dateCreated", "DESC"]],
   filterableColumns: {
     "perfil.id": [PaginationFilter.EQ],
+    "perfil.usuario.matricula": [PaginationFilter.EQ],
+    "perfil.usuario.nome": [PaginationFilter.EQ, PaginationFilter.ILIKE],
+    "perfil.usuario.email": [PaginationFilter.EQ, PaginationFilter.ILIKE],
+    "perfil.campus.id": [PaginationFilter.EQ],
     "curso.id": [PaginationFilter.EQ],
+    "curso.nome": [PaginationFilter.EQ, PaginationFilter.ILIKE],
     periodo: [PaginationFilter.EQ],
+    emailInstitucional: [PaginationFilter.EQ, PaginationFilter.ILIKE],
+    telefone: [PaginationFilter.EQ],
   },
 };
